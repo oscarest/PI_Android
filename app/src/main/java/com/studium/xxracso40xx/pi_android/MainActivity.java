5ec19219d712e03970cf4d2cdc549100f2059c10
Xxracso40xX <Xxracso40xX@gmail.com>
2018-12-25 20:33:35 +0100

    Pantalla login inicial sin funcionalidad

--- app/src/main/java/com/studium/xxracso40xx/pi_android/MainActivity.java
+++ app/src/main/java/com/studium/xxracso40xx/pi_android/MainActivity.java
@@ -1,13 +1,27 @@
 package com.studium.xxracso40xx.pi_android;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView registro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        registro = findViewById(R.id.textView3);
+        registro.setMovementMethod(LinkMovementMethod.getInstance());
+        registro.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW);
+                browserIntent.setData(Uri.parse("http://www.google.com"));
+                startActivity(browserIntent);
+            }
+        });
     }
 }
