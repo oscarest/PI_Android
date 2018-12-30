5f566d3f17ab9a59a4147127847798fe71858e8d
Xxracso40xX <Xxracso40xX@gmail.com>
2018-12-30 15:44:16 +0100

    Correcta conexión a bd local sin introducción de BD. Todo esto es una prueba.

---
+++ app/src/main/java/com/studium/xxracso40xx/pi_android/pruebaMYSQL.java
@@ -0,0 +1,90 @@
+package com.studium.xxracso40xx.pi_android;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+
+public class pruebaMYSQL extends AppCompatActivity {
+
+    private static final String url = "jdbc:mysql://192.168.1.196:3306/tiendecita";
+    private static final String user = "oscar";
+    private static final String pass = "Studium";
+    Button btnFetch,btnClear;
+    TextView txtData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prueba_mysql);
+        txtData = (TextView) this.findViewById(R.id.txtData);
+        btnFetch = (Button) findViewById(R.id.btnFetch);
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnFetch.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+// TODO Auto-generated method stub
+                ConnectMySql connectMySql = new ConnectMySql();
+                connectMySql.execute("");
+            }
+        });
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtData.setText("");
+            }
+        });
+
+    }
+
+    private class ConnectMySql extends AsyncTask<String, Void, String> {
+        String res = "";
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(pruebaMYSQL.this, "Please wait...", Toast.LENGTH_SHORT)
+                    .show();
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection(url, user, pass);
+                System.out.println("Databaseection success");
+
+                String result = "Database Connection Successful\n";
+                Statement st = con.createStatement();
+                ResultSet rs = st.executeQuery("select * from articulos");
+                ResultSetMetaData rsmd = rs.getMetaData();
+
+                while (rs.next()) {
+                    result += rs.getString(1).toString() + "\n";
+                }
+                res = result;
+            } catch (Exception e) {
+                e.printStackTrace();
+                res = e.toString();
+            }
+            return res;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            txtData.setText(result);
+        }
+    }
+
+}
