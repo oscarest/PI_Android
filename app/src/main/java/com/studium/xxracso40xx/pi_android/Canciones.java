a395092f353b3199420036af5cbd622697ec059c
Xxracso40xX <Xxracso40xX@gmail.com>
2019-01-07 18:30:12 +0100

    Botones conectados a sus respectivas activities

---
+++ app/src/main/java/com/studium/xxracso40xx/pi_android/Canciones.java
@@ -0,0 +1,13 @@
+package com.studium.xxracso40xx.pi_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Canciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_canciones);
+    }
+}
--- app/src/main/java/com/studium/xxracso40xx/pi_android/Principal.java
+++ app/src/main/java/com/studium/xxracso40xx/pi_android/Principal.java
@@ -1,13 +1,51 @@
 package com.studium.xxracso40xx.pi_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Principal extends AppCompatActivity {
+public class Principal extends AppCompatActivity
+{
+    Button boton1;
+    Button boton2;
+    Button boton3;
+    Intent intent;
+    Intent intent1;
+    Intent intent2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        boton1 = findViewById(R.id.button3);
+        boton2 = findViewById(R.id.button4);
+        boton3 = findViewById(R.id.button5);
+        intent = new Intent(this, Canciones.class);
+        intent1 = new Intent(this, Nosotros.class);
+        intent2 = new Intent(this, Perfil.class);
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+             startActivity(intent);
+            }
+        });
+        boton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent1);
+
+            }
+        });
+        boton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent2);
+            }
+        });
     }
 }
