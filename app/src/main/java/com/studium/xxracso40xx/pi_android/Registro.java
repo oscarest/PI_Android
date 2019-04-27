cb51af37f100375c97b4a304cc70b51f5902ac6f
juanantonioredondo <redondo.juanantonio1997@gmail.com>
2019-04-27 18:51:35 +0200

    el registro es funcional, pero falta, añadir elementos al insert y validación de campos vacíos

--- app/src/main/java/com/studium/xxracso40xx/pi_android/Registro.java
+++ app/src/main/java/com/studium/xxracso40xx/pi_android/Registro.java
@@ -2,6 +2,7 @@ package com.studium.xxracso40xx.pi_android;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,10 +11,27 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.studium.xxracso40xx.pi_android.model.Usuarios;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Registro extends AppCompatActivity {
-EditText editTextRegistroFechaDeNacimiento, editTextRegistroNombre, editTextRegistroApellidos, editTextRegistroNombreUsuario, editTextRegistroEmail, editTextRegistroDireccion;
+EditText editTextRegistroFechaDeNacimiento, editTextRegistroContrasena, editTextRegistroNombre, editTextRegistroApellidos, editTextRegistroNombreUsuario, editTextRegistroEmail, editTextRegistroDireccion;
 Button buttonCancelar, buttonConfirmar;
 Intent intent1;
+    private String APIserver = "http://8music.ddns.net/webserviceAndroid/";
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +43,7 @@ Intent intent1;
         editTextRegistroNombreUsuario = findViewById(R.id.editTextRegistroNombreUsuario);
         editTextRegistroEmail = findViewById(R.id.editTextRegistroEmail);
         editTextRegistroDireccion = findViewById(R.id.editTextRegistroDireccion);
+        editTextRegistroContrasena = findViewById(R.id.editTextRegistroContrasena);
         buttonCancelar = findViewById(R.id.buttonCancelar);
         buttonConfirmar = findViewById(R.id.buttonConfirmar);
         intent1 = new Intent(this, MainActivity.class );
@@ -36,6 +55,7 @@ Intent intent1;
                 Toast toast = Toast.makeText(getApplicationContext(),"Se ha registrado correctamente", Toast.LENGTH_SHORT);
                 toast.show();
                //Borrar los datos solo si ha funcionado correctamente el registro
+                new Registro.DB_Apache().execute("set-user.php?nick=" + editTextRegistroNombreUsuario.getText().toString() + "&clave=" + editTextRegistroContrasena.getText().toString());
                 editTextRegistroFechaDeNacimiento.setText("");
                 editTextRegistroNombre.setText("");
                 editTextRegistroApellidos.setText("");
@@ -65,6 +85,31 @@ Intent intent1;
         });
 
     }
+
+    private class DB_Apache extends AsyncTask<String, Void, Boolean> {
+
+        private String json;
+
+        protected Boolean doInBackground(String... urls) {
+            try {
+                HttpClient client = new DefaultHttpClient();
+                HttpGet request = new HttpGet();
+                request.setURI(new URI(APIserver + urls[0]));
+                client.execute(request);
+            } catch (Exception e) {
+                System.out.println("FALLO: " + e.getMessage());
+                return false;
+            }
+            return true;
+        }
+
+        protected void onPostExecute(Boolean isOk) {
+
+
+        }
+    }
+
+
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.editTextRegistroFechaDeNacimiento:
@@ -85,5 +130,7 @@ Intent intent1;
         });
         newFragment.show(getSupportFragmentManager(), "datePicker");
     }
+
+
 
 }
